aa2a5da90b9e0d8f8e3ca0de88c09cb052a79639
shubhhampgit01 <shubhhampgit01@gmail.com>
2024-07-25 12:16:31 +0530

    home page

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,4 +1,4 @@
 import  "./pages/navbar/Navbar.module.scss";
 import "./pages/home/HomePage.module.scss";
 export { default as NavBar } from "./pages/navbar/Navbar";
-export {default as HomePage} from "./pages/home/HomePage.module.scss";
+export {default as HomePage} from "./pages/home/index";
